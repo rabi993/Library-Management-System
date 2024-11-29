3bd12b47589d266725e5f3afb6ec1b7aa3d27d83
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2024-11-29 21:42:50 +0600

    url changed

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,16 @@
 
 from django.urls import path
 
-from . import views
+# from . import views
+
+urlpatterns = [
+    # path('homepage/', views.home, name='homepage'),
+    # path('category/<slug:category_slug>/', views.home, name='category_wise_book'),
+]
+
+
 # urlpatterns = [
     
 #     path('home/', views.home, name='homepage'),
 #     path('category/<slug:category_slug>/', views.home, name='category_wise_book'),
 # ]
-
-urlpatterns = [
-    path('home/', views.home, name='homepage'),
-    path('category/<slug:category_slug>/', views.home, name='category_wise_book'),
-]
