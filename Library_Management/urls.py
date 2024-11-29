3f0872e6f7c69032cb29527f775be778e13c28c3
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2024-11-29 22:47:31 +0600

    core app urls edited from library management urls

--- Library_Management/urls.py
+++ Library_Management/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-# from core.views import HomeView, home
+from core.views import HomeView
 
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    # path('', HomeView.as_view(), name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
    
     path('accounts/', include('accounts.urls')),
-    path('', include('core.urls')),
+    path('core/', include('core.urls')),
     path('transactions/', include('transactions.urls')),
     path('books/', include('books.urls')),
 
